423234bd9fe33c16507f2df52a42036474ffa073
Belbos <maplelyy@naver.com>
2018-01-08 23:59:31 +0900

    왼쪽 메뉴 완성, Home, Coin 개발시작

--- blog/views.py
+++ blog/views.py
@@ -7,3 +7,8 @@ def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'blog/post_list.html', {'posts': posts})
     #return render(request, 'blog/post_list.html', {})
+
+
+def Coin(request):
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'blog/Coin.html', {'posts': posts})
